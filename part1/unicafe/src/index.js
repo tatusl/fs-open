76899477f0c97fb7115333466cb36d8ddd9ab40d
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2020-11-03 08:56:15 +0200

    Unicafe (exercise 1.8)

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,12 +7,29 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good+neutral+bad
+
+  return (
+    <>
+    <h1>statistics</h1>
+    <ul>
+      <li>good {good}</li>
+      <li>neutral {neutral}</li>
+      <li>bad {bad}</li>
+      <li>all {all}</li>
+      <li>average {(good*1+neutral*0+bad*-1)/all}</li>
+      <li>positive {(good/all)*100+'%'}</li>
+    </ul>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good+neutral+bad
 
   return (
     <div>
@@ -20,15 +37,7 @@ const App = () => {
       <Button handleClick={() => setGood(good+1)} text="good" />
       <Button handleClick={() => setNeutral(neutral+1)} text="neutral" />
       <Button handleClick={() => setBad(bad+1)} text="bad" />
-      <h1>statistics</h1>
-      <ul>
-        <li>good {good}</li>
-        <li>neutral {neutral}</li>
-        <li>bad {bad}</li>
-        <li>all {all}</li>
-        <li>average {(good*1+neutral*0+bad*-1)/all}</li>
-        <li>positive {(good/all)*100+'%'}</li>
-      </ul>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
