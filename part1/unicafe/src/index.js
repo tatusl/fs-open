098f982433f7a24995b8107e719074d595aa41ee
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2020-11-03 08:39:51 +0200

    Unicafe (exercise 1.7)

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -12,6 +12,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good+neutral+bad
 
   return (
     <div>
@@ -24,6 +25,9 @@ const App = () => {
         <li>good {good}</li>
         <li>neutral {neutral}</li>
         <li>bad {bad}</li>
+        <li>all {all}</li>
+        <li>average {(good*1+neutral*0+bad*-1)/all}</li>
+        <li>positive {(good/all)*100+'%'}</li>
       </ul>
     </div>
   )
