ab9cc3ee87d6b5a87d35d3a516624102921e55ed
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2020-11-15 14:50:51 +0200

    Phonebook (exercise 2.6)

---
+++ part2/phonebook/src/components/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const listStyle = {
+    listStyle: 'none',
+    paddingLeft: 0
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul style={listStyle}>
+      {persons.map(person =>
+        <li key={person.name}>
+          {person.name}
+        </li>
+      )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
