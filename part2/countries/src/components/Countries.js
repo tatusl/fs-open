6a4f2d82f70491b5fa30efe1592a29446e6fcbed
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2020-11-22 22:58:54 +0200

    Countries: exercie 2.13

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BasicCountryData from './BasicCountryData'
 
-const countryListStyle = {
-  listStyle: 'none',
-  paddingLeft: 0
-}
-
 const Countries = ({ countries, countryFilter }) => {
+  const [visibleCountries, setVisibleCountries] = useState([])
+
   const filteredCountries = countries.filter(country => country.name.match(new RegExp(countryFilter, 'gi')))
+
+  /*
+   * Handles state change of visibleCountries according to button presses.
+   * If country does not exist in array, adds the country to the array.
+   * If country does exist in array, removes the country from the array
+   */
+  const handleShowCountryInfo = (country) => {
+    const copy = [...visibleCountries].includes(country)
+      ? [...visibleCountries].filter(i => i !== country)
+      : [...visibleCountries, country]
+    setVisibleCountries(copy)
+  }
 
   if (filteredCountries.length > 10) {
     return (
@@ -23,15 +32,17 @@ const Countries = ({ countries, countryFilter }) => {
   }
 
   return (
-    <ul style={countryListStyle}>
-    {filteredCountries
-      .map(country =>
-        <li key={country.name}>
-          {country.name}
-        </li>
-      )
-    }
-    </ul>
+    <div>
+      {filteredCountries
+        .map(country =>
+          <React.Fragment key={country.name}>
+            {country.name}
+            <button onClick={() => handleShowCountryInfo(country.name)}>show</button><br />
+            {visibleCountries.includes(country.name) && <BasicCountryData country={country} />}
+          </React.Fragment>
+        )
+      }
+    </div>
   )
 }
 
