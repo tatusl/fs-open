ec3f1562b07199bd492a47cf57f2ba9d91839f9f
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2020-11-25 19:22:38 +0200

    Phonebook backend: exercise 3.6

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -47,9 +47,21 @@ app.get('/api/persons/:id', (request, response) => {
 app.post('/api/persons/', (request, response) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
+  if (!body.name) {
     return response.status(400).json({
-      error: 'name and/or number missing',
+      error: 'name is missing',
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number is missing',
+    })
+  }
+
+  if (persons.some((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique',
     })
   }
 
