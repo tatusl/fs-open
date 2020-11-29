e2394d9930e64cd97b90dd256acb2273e3680fed
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2020-11-29 23:05:20 +0200

    Phonebook backend: exercise 3.17

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -111,6 +111,21 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch((error) => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.get('/info', (request, response) => {
   const response_msg = `Phonebook has info for ${persons.length} people <br/>
     <br/>
