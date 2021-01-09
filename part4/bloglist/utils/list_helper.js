e01904e507b615b65554a090ddc3d2cdda4c2d8f
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2021-01-09 20:49:27 +0200

    Bloglist: Exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sum = blogs.reduce((sum, item) => sum + item.likes, 0)
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
