e9384eece8131ae9ed05c3c3548b56b4d30dcd0b
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2021-01-11 20:02:13 +0200

    Bloglist: More compact function form

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,26 +1,21 @@
 const _ = require('lodash')
 
-const dummy = (blogs) => {
-  return 1
-}
+const dummy = (blogs) => 1
 
-const totalLikes = (blogs) => {
-  const sum = blogs.reduce((sum, item) => sum + item.likes, 0)
-  return sum
-}
+const totalLikes = (blogs) => blogs
+  .reduce((sum, item) => sum + item.likes, 0)
 
-const mostBlogs = (blogs) => _(blogs)
-  .countBy('author')
-  .toPairs()
-  .map((blog) => ({ author: blog[0], blogs: blog[1] }))
-  .maxBy('blogs')
+const mostBlogs = (blogs) =>
+  _(blogs)
+    .countBy('author')
+    .toPairs()
+    .map((blog) => ({ author: blog[0], blogs: blog[1] }))
+    .maxBy('blogs')
 
-const favoriteBlog = (blogs) => {
-  return blogs.reduce(
-    (max, blog) => (max && max.likes > blog.likes ? max : blog),
+const favoriteBlog = (blogs) => blogs
+  .reduce((max, blog) => (max && max.likes > blog.likes ? max : blog),
     undefined
   )
-}
 
 module.exports = {
   dummy,
